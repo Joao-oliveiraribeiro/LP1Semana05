5e4730f1c62d3feb835ec23b5c46103352de3409
Joao-oliveiraribeiro <115244204+Joao-oliveiraribeiro@users.noreply.github.com>
2023-03-31 11:18:28 +0100

    Add question of number of throwns and the sum of all the numbers

---
+++ RandomDice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RandomDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantos dados quer lancar?");
+            int n=Convert.ToInt32(Console.ReadLine());
+            int soma1 =0;
+            Random r1 = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                int n1= r1.Next(1,7);
+                Console.WriteLine(n1);
+                soma1=soma1+n1;
+            }
+            Console.WriteLine("Total da soma dos dados e: " + soma1);
+
+        }
+    }
+}
