cb6d37c9cd6860a2fecd6e403e696e7af1d654f8
Joao-oliveiraribeiro <115244204+Joao-oliveiraribeiro@users.noreply.github.com>
2025-03-20 22:06:04 +0000

    Add class doc

--- LevelOne/MainIsHere.cs
+++ LevelOne/MainIsHere.cs
@@ -1,12 +1,23 @@
-﻿using System;
+using System;
 
 namespace LevelOne
 {
+    /// <summary>
+    /// Program starts here and runs
+    /// </summary>
     public class Program
     {
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP1!");
         }
+        /// <summary>
+        /// </summary>
+        public static void NewFeature()
+        {
+            Console.WriteLine("New Feature!");
+        }
     }
+
+
 }
