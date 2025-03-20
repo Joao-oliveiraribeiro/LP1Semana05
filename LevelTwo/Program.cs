e2fda1836227c433c8ed18f82b7447bdf807159a
Joao-oliveiraribeiro <115244204+Joao-oliveiraribeiro@users.noreply.github.com>
2025-03-20 22:36:54 +0000

    Add code for default setting and verifications of arguments

---
+++ LevelTwo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LevelTwo
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string nomeImagem = "tux.jpg";
+            int largura = 24;
+
+            if (args.Length >=1)
+            {
+                nomeImagem = args[0];
+
+            }
+            if (args.Length >=2)
+            {
+                largura = int.Parse(args[1]);
+            }
+        }
+    }
+}
