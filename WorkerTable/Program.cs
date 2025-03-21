a5e718dc19767b8cbab6f3b37bbe80bb4c2df42d
Joao-oliveiraribeiro <115244204+Joao-oliveiraribeiro@users.noreply.github.com>
2025-03-21 15:00:27 +0000

    Add code for table

--- WorkerTable/Program.cs
+++ WorkerTable/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Spectre.Console;
+using Bogus;
 
 namespace WorkerTable
 {
@@ -8,15 +9,23 @@ namespace WorkerTable
         private static void Main(string[] args)
         {
             Randomizer.Seed = new Random(int.Parse(args[0]));
+            int workers = int.Parse(args[0]);
             Faker faker = new Faker("pt_PT");
-
+            int base_number = 1
+            
+            
             var table = new Table();
 
             table.AddColumn("ID");
             table.AddColumn(new TableColumn("Name"));
             table.AddColumn(new TableColumn("Job"));
 
-
+            while (base_number <= workers)
+            {
+                table.AddRow(new Markup(base_number, faker.FullName, faker.JobTitle));
+                base_number++;
+                
+            }
 
 
             AnsiConsole.Write(table);
